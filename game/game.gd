a8cbc83e88ae380aa62908910c26f221f9168cc1
Jasper Mutsaerts <jasper.mutsaerts@paqt.com>
2026-06-21 13:00:23 +0200

    added movement

--- game/game.gd
+++ game/game.gd
@@ -5,10 +5,10 @@ var frog_scene:PackedScene = preload("res://player/frog.tscn")
 
 var frog:Frog
 
+@onready var game_layer:Node2D = $GameLayer
+@onready var player_spawn_point:Node2D = %PlayerSpawnPoint
+
 func _ready() -> void:
-	frog = frog_scene.instantiate() as Frog
-	add_child(frog)
-
-
-
-
+	frog = frog_scene.instantiate()
+	frog.global_position = player_spawn_point.global_position
+	game_layer.add_child(frog)
--- player/frog.gd
+++ player/frog.gd
@@ -2,11 +2,29 @@ extends CharacterBody2D
 class_name Frog
 
 
+const MOVE_SPEED = 100
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	velocity = Vector2(0, 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_up"):
+		position.y -= MOVE_SPEED
+		rotation = 0
+	elif Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+		var direction = Input.get_axis("ui_left", "ui_right")
+		position.x += MOVE_SPEED * direction
+		rotation = PI  *.5 * direction
+
+
+	var collision = move_and_collide(Vector2(0, 0), true)
+
+	if collision:
+		print("hit something")
+
+
+
 	pass
